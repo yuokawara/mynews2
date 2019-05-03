de1e838e3bc88d7e1a9c7ea0122647443c31a120
yuokawara <solbonta1@gmail.com>
2019-05-03 12:17:06 +0900

    作業内容4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,11 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="login-box card">
-                    <div class="login-header card-header mx-auto">{{ __('Login') }}</div>
+                    <div class="login-header card-header mx-auto">{{ __('message.ログイン') }}</div>
 
                     <div class="login-body card-body">
                         <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                            {{ csrf_field() }}
 
                             <div class="form-group row">
                                 <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
@@ -52,7 +52,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
+                                        {{ __('message.ログイン') }}
                                     </button>
                                 </div>
                             </div>
