7e47257ffe6e93af21894e9bb1d996624f154968
大河原悠 <solbonta1@gmail.com>
2019-06-09 22:38:12 +0900

    作業内容8

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -8,6 +8,8 @@
                 <h2>ニュース新規作成</h2>
                 <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
 
+ // 以下を追記
+
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
