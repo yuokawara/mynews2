c2bbe69c426ce6004a2f2bcbbedc3ceece636ce0
大河原悠 <solbonta1@gmail.com>
2019-06-29 13:28:47 +0900

    Revert "text部分余分な箇所修正"
    This reverts commit 11557cbb57fb759b6f421e24785121c4d3b46bf5.

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -24,7 +24,9 @@
               <div class="form-group row">
                 <label class="col-md-2" for="title">本文</label>
                 <div class="col-md-10">
-                  <textarea class="form-control" name="body" rows="20">{{ $news_form->body }}</textarea>
+                  <textarea class="form-control" name="body" rows="20">
+                    {{ $news_form->body }}
+                  </textarea>
                 </div>
               </div>
               <div class="form-group row">
