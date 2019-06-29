37361de328f4f9ab74a85896f4b430b0d2db5cee
大河原悠 <solbonta1@gmail.com>
2019-06-29 20:31:10 +0900

    作業内容18修正

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -3,7 +3,6 @@
 
 @section('content')
     <div class="container">
-<<<<<<< HEAD
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>ニュース編集</h2>
@@ -47,38 +46,6 @@
                             {{ csrf_field() }}
                             <input type="submit" class="btn btn-primary" value="更新">
                         </div>
-=======
-      <div class="row">
-        <div class="col-md-8 mx-auto">
-          <h2>ニュース編集</h2>
-          <form action="{{ action('Admin\NewsController@update')}}" method="post" enctype="multipart/form-data">
-              @if (count($errors) > 0)
-                  <ul>
-                    @foreach($errors->all() as $e)
-                        <li>{{ $e }}</li>
-                    @endforeach
-                  </ul>
-              @endif
-              <div class="form-group row">
-                <label class="col-md-2" for="title">タイトル</label>
-                <div class="col-md-10">
-                  <input type="text" class="form-control"
-                   name="title" value="{{ $news_form->title }}">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-md-2" for="title">本文</label>
-                <div class="col-md-10">
-                  <textarea class="form-control" name="body" rows="20">{{ $news_form->body }}</textarea>
-                </div>
-              </div>
-              <div class="form-group row">
-                  <label class="col-md-2">画像</label>
-                  <div class="col-md-10">
-                    <input type="file" class="form-control-file" name="image">
-                    <div class="form-text text-info">
-                      設定中: {{ $news_form->image_path }}
->>>>>>> develop
                     </div>
                 </form>
                 {{-- 以下を追記　--}}
