645309431f7a3692f2c88f5cdfe536133450e16c
大河原悠 <solbonta1@gmail.com>
2019-06-30 17:16:58 +0900

    作業内容21heroku実装

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\HTML;
+
+// 追記
+use App\News;
+
+class NewsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        // $cond_title が空白でない場合は、記事を検索して取得する
+        if ($cond_title != '') {
+            $posts = News::where('title', $cond_title).orderBy('updated_at', 'desc')->get();
+        } else {
+            $posts = News::all()->sortByDesc('updated_at');
+        }
+
+        if (count($posts) > 0) {
+            $headline = $posts->shift();
+        } else {
+            $headline = null;
+        }
+
+        // news/index.blade.php ファイルを渡している
+        // また View テンプレートに headline、 posts、 cond_title という変数を渡している
+        return view('news.index', ['headline' => $headline, 'posts' => $posts, 'cond_title' => $cond_title]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
