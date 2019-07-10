f082ceeb2208aae3172d8b79817d48dfa1584fdb
大河原悠 <solbonta1@gmail.com>
2019-07-10 15:25:42 +0900

    作業内容7AWS稼働後修正

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -64,9 +64,9 @@
             <div class="col-md-6 mx-auto">
               <h2>プロフィール編集履歴</h2>
                 <ul class="list-group">
-                  @if ($profile_form->profile_histories != NULL)
-                    @foreach ($profile_form->profile_histories as $profilehistory)
-                      <li class="list-group-item">{{ $profile_histories->edited_at }}</li>
+                  @if ($profile_form->profilehistories != NULL)
+                    @foreach ($profile_form->profilehistories as $profilehistory)
+                      <li class="list-group-item">{{ $profilehistory->edited_at }}</li>
                     @endforeach
                   @endif
                 </ul>
